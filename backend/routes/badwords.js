0751f7c88ad8e477fcced789a13c4950ffe9637d
Dung Le <dvl15@case.edu>
2019-11-21 16:50:03 -0500

    Fix npm start and link to file txt

--- backend/routes/badwords.js
+++ backend/routes/badwords.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 var file = fs.readFileSync('/home/lnp26/github/campuskvetch/backend/routes/badwords.txt').toString('utf-8')
+//var file = fs.readFileSync('D://campuskvetch/backend/routes/badwords.txt').toString('utf-8')
 var badWords = file.split(', ')
 
 module.exports = badWords
